5cc7aa0cebbd9bad6f1893d2c0161873fe2504e2
Trira <trubchik@mail.ru>
2026-02-01 19:32:39 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MainPage from './components/MainPage';
+import TestPage from './components/TestPage';
+import EditTestPage from './components/EditTestPage';
+import './styles.css';
 
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/" exact component={MainPage} />
+          <Route path="/test" component={TestPage} />
+          <Route path="/edit" component={EditTestPage} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
