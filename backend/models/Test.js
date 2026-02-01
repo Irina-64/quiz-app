78589050ac97306eb88200972bb99aa31cf5c1d1
Trira <trubchik@mail.ru>
2026-02-01 19:31:13 +0300

    Update Test.js

--- backend/models/Test.js
+++ backend/models/Test.js
@@ -1,1 +1,34 @@
+const mongoose = require('mongoose');
 
+const answerSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true
+  },
+  isCorrect: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const questionSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true
+  },
+  answers: [answerSchema]
+});
+
+const testSchema = new mongoose.Schema({
+  questions: [questionSchema],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Test', testSchema);
