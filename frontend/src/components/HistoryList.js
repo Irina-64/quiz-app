786709b259b89b6d40c207feded9fe374316040b
Trira <trubchik@mail.ru>
2026-02-01 19:36:39 +0300

    Update HistoryList.js

--- frontend/src/components/HistoryList.js
+++ frontend/src/components/HistoryList.js
@@ -1,1 +1,35 @@
+import React from 'react';
 
+const HistoryList = ({ history }) => {
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('ru-RU') + ' ' + date.toLocaleTimeString('ru-RU');
+  };
+  
+  return (
+    <div className="history-list">
+      {history.map((item, index) => (
+        <div key={index} className="history-item">
+          <div className="history-date">{item.date}</div>
+          <div className="history-details">
+            <span className="history-stat">Вопросов: {item.totalQuestions}</span>
+            <span className="history-stat">Верно: {item.correctAnswers}</span>
+          </div>
+          <div className="history-progress">
+            <div className="progress-bar">
+              <div 
+                className="progress-fill" 
+                style={{ width: `${(item.correctAnswers / item.totalQuestions) * 100}%` }}
+              ></div>
+            </div>
+            <span className="progress-text">
+              {Math.round((item.correctAnswers / item.totalQuestions) * 100)}%
+            </span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default HistoryList;
